4f7ec3b8461b3782231d8dcb85376fddf0bc504b
NastyPigz <williamzwy06701@gmail.com>
2022-07-01 20:18:34 -0400

    everything

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
 echo starting...
-cd java/bukkit_command && java -Xmx480M -Xms480M -jar craftbukkit-1.5.2-R1.0.jar
-(cd java/bukkit_command && java -Xmx480M -Xms480M -jar craftbukkit-1.5.2-R1.0.jar) & (cd java/bungee_command && java -Xmx32M -Xms32M -jar bungee-dist.jar)
+cd java/bungee_command
+java -Xmx32M -Xms32M -jar bungee-dist.jar > /dev/null &2>1 &
+cd java/bukkit_command
+java -Xmx480M -Xms480M -jar craftbukkit-1.5.2-R1.0.jar 
