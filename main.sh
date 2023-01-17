ed4180723f819b75e24c326573be1ade6df7a427
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-01-16 22:30:49 -0500

    update

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 echo starting...
 cd java/bungee_command
-java -Xmx32M -Xms32M -jar bungee-dist.jar > /dev/null &2>1 &
+java -Xmx64M -Xms64M -jar bungee-dist.jar > /dev/null &2>1 &
 cd java/bukkit_command
-java -Xmx480M -Xms480M -jar craftbukkit-1.5.2-R1.0.jar 
+java -Xmx512M -Xms512M -jar craftbukkit-1.5.2-R1.0.jar 
